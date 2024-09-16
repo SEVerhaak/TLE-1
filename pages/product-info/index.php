e86ddd381c61726ed0a6d15635b4cafd35795b64
ElisaZornig <elisazornig@gmail.com>
2024-09-16 15:15:31 +0200

    product-info

---
+++ pages/product-info/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../css/style.css">
+    <title>Product-info</title>
+</head>
+<body>
+<h1>Naam product</h1>
+<p>Description</p>
+<p>Producent: producer</p>
+<p>diet-info: IF STATEMENT alleen als deze info er is</p>
+<p>Materialen: materialen</p>
+<p>Eco-impact: getal</p>
+</body>
+</html>
