c180915feecb59102f53873377a3a84db3f9eecb
ElisaZornig <elisazornig@gmail.com>
2024-09-16 20:34:12 +0200

    product-info met api

--- pages/product-info/index.php
+++ pages/product-info/index.php
@@ -1,3 +1,10 @@
+<?php
+if (isset($_GET['ean'])) {
+    $ean = $_GET['ean'];
+} else {
+    header("Location: ../scanner/");
+}
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -8,12 +15,44 @@
     <link rel="stylesheet" href="../../css/style.css">
     <title>Product-info</title>
 </head>
+<header>
+    <div id="meta-data-ean" style="display: none;"><?php echo $ean ?> </div>
+</header>
 <body>
-<h1>Naam product</h1>
-<p>Description</p>
+<h1 id = "product-name"></h1>
+<img id = "product-image" >
+<p id = "nutri-score"></p>
 <p>Producent: producer</p>
 <p>diet-info: IF STATEMENT alleen als deze info er is</p>
 <p>Materialen: materialen</p>
 <p>Eco-impact: getal</p>
 </body>
 </html>
+<script>
+    let ean = document.getElementById('meta-data-ean').innerHTML;
+    let url = `https://world.openfoodfacts.org/api/v2/product/${ean}.json`;
+    console.log(url)
+    // Gebruik de fetch-API om de data op te halen
+    fetch(url)
+        .then(response => {
+            // Controleer of het verzoek succesvol was
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            // Converteer de response naar JSON
+            return response.json();
+        })
+        .then(data => {
+            if (data && data.product) {
+                document.getElementById('product-name').innerHTML = `${data.product.product_name || 'N/A'}`;
+                document.getElementById('nutri-score').innerHTML = `Nutri-score: ${data.product.nutriscore_2021_tags || 'N/A'}`;
+                document.getElementById('product-image').src = `${data.product.image_front_small_url || 'N/A'}`;
+            } else {
+                console.error('Product data not found');
+            }
+        })
+        .catch(error => {
+            // Foutafhandeling als het verzoek mislukt
+            console.error('Er is een fout opgetreden:', error);
+        });
+</script>
