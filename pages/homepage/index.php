32e99c4714650118141612e94e826ec53d282669
ElisaZornig <elisazornig@gmail.com>
2024-09-16 14:53:32 +0200

    products.sql

---
+++ pages/homepage/index.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../css/style.css">
+    <title>Homepage</title>
+</head>
+<body>
+<h1>Welkom bij EcoJourney!</h1>
+<p>Scan je producten en kom achter alle productspecificaties!</p>
+<p>Kom ook achter de Eco-score van alle producten</p>
+<a href = "../scanner"><button>Click here!</button></a>
+</body>
+</html>
