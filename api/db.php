b7dd9039eaaab98028b01a62ecb45e18ce842a46
ElisaZornig <elisazornig@gmail.com>
2024-09-18 15:06:04 +0200

    database op server gezet

--- api/db.php
+++ api/db.php
@@ -1,8 +1,8 @@
 <?php
 $host       = "localhost";
-$database   = "tle_1";
-$user       = "root";
-$password   = "";
+$database = "1068035";
+$user =  "1068035";
+$password = "eeloquie";
 
 $db = mysqli_connect($host, $user, $password, $database)
 or die("Error: " . mysqli_connect_error());
