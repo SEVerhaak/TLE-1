b8193565ecd2e2cf765294e03fe3450002d5c54c
ElisaZornig <elisazornig@gmail.com>
2024-09-23 14:20:53 +0200

    main center

--- pages/account/login.php
+++ pages/account/login.php
@@ -48,11 +48,17 @@ if (isset($_POST['submit'])) {
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel = "stylesheet" href="../../css/style.css">
+        <link rel="stylesheet" href="../../css/elisa.css">
         <title>Login</title>
     </head>
     <header>
 
     </header>
+    <style>
+        body{
+            background-color: white;
+        }
+    </style>
     <body>
     <?php include('../../includes/nav.php'); ?>
     <main>
