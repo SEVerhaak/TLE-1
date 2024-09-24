ee8913df34ca6cb88d82f6bac1765340a4349820
basverhaak <severhaak@hotmail.nl>
2024-09-24 11:25:27 +0200

    Product pagina (bijna!) af v2

--- pages/product-info/layout.php
+++ pages/product-info/layout.php
@@ -1,4 +1,20 @@
 <?php
+//require_once "../../api/db.php";
+///** @var mysqli $db */
+session_start();
+if (isset($_GET['ean'])) {
+    $ean = $_GET['ean'];
+    if (isset($_SESSION['users_id'])) {
+        $id = $_SESSION['users_id'];
+    } else{
+        $id = '';
+    }
+//    $ean = mysqli_real_escape_string($db, $_GET['ean']);
+    // Controleer of er al een array voor EAN's in de sessie bestaat, zo niet, maak er een
+} else {
+    //header('location: ../homepage');
+    echo 'no ean given :(';
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -10,6 +26,7 @@
     <title>Layout</title>
     <link rel="stylesheet" href="../../css/style.css">
     <link rel="stylesheet" href="../../css/bas.css">
+    <link rel="stylesheet" href="../../css/elisa.css">
 </head>
 <style>
     .container{
@@ -44,6 +61,8 @@
     .image-container{
         padding: 1vw;
         border-radius: 1.5rem;
+        width: 85vw;
+        background-color: rgba(149, 149, 149, 0.5);
     }
 
     .eco-score-container{
@@ -53,6 +72,7 @@
         border-radius: 1.5rem;
         top: -2rem;
         position: relative;
+        box-shadow: 0 0 10px 5px rgba(0, 0, 0, 0.1);
     }
 
     img{
@@ -96,14 +116,18 @@
         font-weight: bold;
     }
 
-
+    .accordion-wrapper{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 
     .accordion {
         background-color: white;
         color: #444;
         cursor: pointer;
         padding: 6vw;
-        width: 100%;
+        width: 85vw;
         border: none;
         text-align: left;
         outline: none;
@@ -121,9 +145,10 @@
 
     .panel {
         margin-top: -2vw;
-        padding: 0 8vw;
+        padding: 0;
         display: none;
         background-color: white;
+        width: 85vw;
         overflow: hidden;
         border-radius: 0 0 0.5rem 0.5rem;
         transition: all 0.3s ease-in-out;
@@ -139,7 +164,7 @@
         right: 0;
         margin-inline: auto;
         width: 85%;
-        bottom: 6rem;
+        bottom: 7rem;
         font-size: 1.1rem;
         box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
     }
@@ -150,21 +175,27 @@
     }
 
 </style>
+<header>
+    <div id="meta-data-ean" style="display: none;"><?php echo $ean ?> </div>
+    <div id="meta-data-id" style="display: none"><?= $id ?></div>
+</header>
 <body>
+<?php include('../../includes/nav.php'); ?>
+
 <main>
     <section class="container background-color-3" >
         <div class="image-container color-7">
-            <img src="../../images/placeholder.webp"
+            <img id='product-image' src="../../images/placeholder.webp"
         </div>
     </section>
     <div class="eco-score-container eco-color-grey">
         <div class="eco-score-flex-container">
-            <h2 class="product-title">Lorem Ipsum</h2>
+            <h2 id='product-name' class="product-title">Lorem Ipsum</h2>
             <div class="eco-score-img-text-container">
                 <div class="text-stacker">
-                    <p class="bold">Lorem Ipsum:</p>
+                    <p class="bold">Gewicht:</p>
                     <p>Lorem Ipsum</p>
-                    <p class="bold">Lorem Ipsum:</p>
+                    <p class="bold">CO2 per 100gr:</p>
                     <p>Lorem Ipsum</p>
                 </div>
                     <img class='eco-score-img' src="https://static.openfoodfacts.org/images/attributes/dist/ecoscore-unknown.svg">
@@ -172,26 +203,28 @@
         </div>
     </div>
     <div style="display: flex; margin-top: 1rem; margin-bottom: 1rem">
-        <h3>Categorie 1, </h3>
-        <h3>Categorie 2, </h3>
-        <h3>Categorie 3</h3>
+        <h3 id="categories" style="font-weight: lighter">Categorie 1, </h3>
     </div>
-    <div>
-        <button class="accordion">Section 1</button>
+    <div class="accordion-wrapper">
+        <button class="accordion">CO2 Informatie</button>
         <div class="panel">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <ul>
+            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+            </ul>
         </div>
 
-        <button class="accordion">Section 2</button>
+        <button class="accordion">Verpakking Informatie</button>
         <div class="panel">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        </div>
+            <ul>
+                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+            </ul>        </div>
 
-            <button class="accordion">Section 3</button>
+            <button class="accordion">Transport Informatie</button>
 
         <div class="panel">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        </div>
+            <ul>
+                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+            </ul>        </div>
     </div>
     <button class="save-info eco-color-grey">
         <h3> bewaar zoekopdracht </h3>
@@ -199,10 +232,19 @@
     <div style="margin-top: 20rem"></div>
 
 </main>
+<?php include('../../includes/footer.php'); ?>
+
 </body>
 <script>
     let accordion = document.getElementsByClassName("accordion");
+    const ean = document.getElementById('meta-data-ean').innerHTML;
+    const userId =  document.getElementById('meta-data-id').textContent;
+    let url = `https://world.openfoodfacts.org/api/v2/product/${ean}.json`;
 
+    document.addEventListener("load", fetchResults);
+
+
+    // voor de dropdown accordion menu's
     for (let i = 0; i < accordion.length; i++) {
         accordion[i].addEventListener("click", function() {
             console.log(this)
@@ -216,5 +258,192 @@
             }
         });
     }
+
+    // Gebruik de fetch-API om de data op te halen
+
+    function fetchResults(){
+        console.log('start of fetch')
+        fetch(url)
+            .then(response => {
+                // Controleer of het verzoek succesvol was
+                if (!response.ok) {
+                    window.location.href = '../scanner'
+                    throw new Error('Network response was not ok');
+                }
+                // Converteer de response naar JSON
+                console.log('fetch complete')
+                return response.json();
+            })
+            .then(data => {
+                if (data && data.product) {
+                    console.log('handling data')
+
+                    localStorage.setItem('data', JSON.stringify(data.product))
+
+                    if (localStorage.getItem('data')){
+                        dataHandler(JSON.parse(localStorage.getItem('data')))
+                    } else{
+                        dataHandler(data)
+                        let name
+                        if (data.product.brands && data.product.product_name){
+                            name = `${data.product.brands} - ${data.product.product_name}`
+                            saveToHistory(ean, name, userId)
+                        } else{
+                            name = 'N.A'
+                            saveToHistory(ean, name, userId)
+                        }
+                    }
+
+                } else {
+                    window.location.href = '../scanner'
+                }
+            })
+            .catch(error => {
+                // Foutafhandeling als het verzoek mislukt
+                console.error('Er is een fout opgetreden:', error);
+            });
+    }
+
+
+    function saveToHistory(ean, name, id){
+        if (userId !== '' && userId !== undefined && userId){
+            console.log('saving to history!')
+            const url = `../../api/history-add.php?ean=` + encodeURIComponent(`${ean}`) + `&name=` + encodeURIComponent(`${name}`) + `&id=` + encodeURIComponent(`${id}`)
+            fetch(url)
+                .then(response => {
+                    // Controleer of het verzoek succesvol was
+                    if (!response.ok) {
+                        //window.location.href = '../scanner'
+                        throw new Error('Network response was not ok');
+                    }
+                    // Converteer de response naar JSON
+                    return response.json();
+                })
+                .then(data => {
+
+                })
+                .catch(error => {
+                    // Foutafhandeling als het verzoek mislukt
+                    console.error('Er is een fout opgetreden:', error);
+                });
+        }
+    }
+
+    function dataHandler(data) {
+        console.log(data);
+        if (data.product.brands !== undefined && data.product.brands !== '' && data.product.brands !== null) {
+            if (data.product.product_name !== undefined && data.product.product_name !== '' && data.product.product_name !== null) {
+                document.getElementById('product-name').innerHTML = `${data.product.brands} - ${data.product.product_name}`;
+            } else {
+                document.getElementById('product-name').innerHTML = `${data.product.brands}`;
+            }
+        } else if (data.product.product_name !== undefined && data.product.product_name !== '' && data.product.product_name !== null) {
+            document.getElementById('product-name').innerHTML = `${data.product.product_name}`;
+        } else {
+            document.getElementById('product-name').innerHTML = `N.A`;
+        }
+
+
+        if (data.product.image_front_small_url !== undefined && data.product.image_front_small_url !== '' && data.product.image_front_small_url !== null) {
+            document.getElementById('product-image').src = `${data.product.image_front_small_url}`;
+        } else {
+            document.getElementById('product-image').src = `../../images/placeholder.webp`;
+        }
+
+        if (data.product.categories !== undefined && data.product.categories !== '' && data.product.categories !== null) {
+            document.getElementById('categories').innerHTML = `Categorieën: ${data.product.categories}`;
+        } else {
+            console.log('categorien')
+            document.getElementById('categories').innerHTML = 'Geen categorieën gevonden'
+        }
+
+        /*
+
+        if (data.product.product_quantity !== undefined && data.product.product_quantity !== '' && data.product.product_quantity !== null) {
+            if (data.product.product_quantity_unit !== undefined && data.product.product_quantity_unit !== '' && data.product.product_quantity_unit !== null) {
+                document.getElementById('quantity').innerHTML = `Hoeveelheid: ${data.product.product_quantity} ${data.product.product_quantity_unit}`;
+            } else {
+                document.getElementById('quantity').innerHTML = `Hoeveelheid: ${data.product.product_quantity}`;
+            }
+        } else {
+            console.log(`Geen gewicht bekend voor dit product`)
+            document.getElementById('quantity').innerHTML = `Geen gewicht bekend voor dit product`;
+        }
+
+        if (data.product.nutriscore_2023_tags[0] !== '' && data.product.nutriscore_2023_tags[0] !== null && data.product.nutriscore_2023_tags[0] !== undefined) {
+            document.getElementById('nutri-score-image').src = `https://static.openfoodfacts.org/images/attributes/dist/nutriscore-${data.product.nutriscore_2023_tags[0]}-new-en.svg`;
+        } else if (data.product.nutriscore_2021_tags[0] !== '' && data.product.nutriscore_2021_tags[0] !== null && data.product.nutriscore_2021_tags[0] !== undefined) {
+            document.getElementById('nutri-score-image').src = `https://static.openfoodfacts.org/images/attributes/dist/nutriscore-${data.product.nutriscore_2021_tags[0]}-new-en.svg`;
+        } else {
+            document.getElementById('nutri-score-image').src = `https://static.openfoodfacts.org/images/attributes/dist/nutriscore-unknown-new-en.svg`;
+        }
+
+        if (data.product.ingredients_analysis_tags !== undefined) {
+            for (let i = 0; i < 3; i++) {
+                try {
+                    let newString = data.product.ingredients_analysis_tags[i].slice(3)
+                    if (newString !== '' && newString !== null && newString !== undefined) {
+                        document.getElementById(`ingredienttag_${i}`).innerHTML = `${newString}`;
+                    } else {
+                        document.getElementById(`ingredienttag_${i}`).innerHTML = `Geen data`;
+                    }
+                } catch (e) {
+                    console.error('Error slicing: ' + e)
+                }
+            }
+        } else {
+            document.getElementById(`ingredienttagwrapper`).style.display = 'none';
+        }
+
+
+        if (data.product.ecoscore_grade !== '' && data.product.ecoscore_grade !== null && data.product.ecoscore_grade) {
+            document.getElementById('ecoscore-image').src = `https://static.openfoodfacts.org/images/attributes/dist/ecoscore-${data.product.ecoscore_grade}.svg`;
+        } else {
+            document.getElementById('ecoscore-image').src = `https://static.openfoodfacts.org/images/attributes/dist/ecoscore-unknown.svg`;
+        }
+
+        if (data.product.ecoscore_score !== undefined && data.product.ecoscore_score !== '') {
+            document.getElementById('ecoscore-score').innerHTML = `Ecoscore: ${data.product.ecoscore_score}%`;
+            if (data.product.ecoscore_data.agribalyse.co2_total !== undefined && data.product.ecoscore_data.agribalyse.co2_total !== '') {
+                document.getElementById('co2-score').innerHTML = `CO2-score: ${Math.round(data.product.ecoscore_data.agribalyse.co2_total * 100)} gram CO2 uitstoot per 100 gram product`;
+            } else {
+                document.getElementById('co2-score').innerHTML = ``;
+            }
+        } else {
+            document.getElementById('ecoscore-score').innerHTML = `Ecoscore: Onbekend`;
+        }
+
+        if (data.product.packaging !== undefined && data.product.packaging !== '') {
+            document.getElementById('packaging').innerHTML = `Verpakking: ${data.product.packaging}`;
+        } else {
+            document.getElementById('packaging').innerHTML = `Verpakking: Onbekend`;
+        }
+
+        if (data.product.packaging_recycling_tags !== undefined && data.product.packaging_recycling_tags !== '' && data.product.packaging_recycling_tags.length !== 0) {
+            document.getElementById('recycling').innerHTML = `Recycling: ${data.product.packaging_recycling_tags}`;
+        } else {
+            document.getElementById('recycling').innerHTML = `Recycling: Onbekend`;
+        }
+
+        if (data.product.origins !== undefined && data.product.origins !== '') {
+            document.getElementById('transport').innerHTML = `Transport: ${data.product.origins}`;
+        } else {
+            document.getElementById('transport').innerHTML = `Transport: Onbekend`;
+        }
+
+        if (data.product.image_ingredients_small_url !== undefined && data.product.image_ingredients_small_url !== '') {
+            document.getElementById('ingredients-image').src = `${data.product.image_ingredients_small_url}`;
+        } else {
+            document.getElementById('ingredients-image').src = ``;
+        }
+         */
+
+
+        //document.getElementById('nutri-score').innerHTML = `Nutri-score: ${data.product.nutriscore_2021_tags || 'N/A'}`;
+
+        console.log(data.product.packaging_recycling_tags.length)
+
+    }
+
 </script>
 </html>
