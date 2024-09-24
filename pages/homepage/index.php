cab5de2614da41c93480d8a989453a651979ce77
IsisvanDijk <144005645+IsisvanDijk@users.noreply.github.com>
2024-09-24 10:34:06 +0200

    img toegevoegd aan git

--- pages/homepage/index.php
+++ pages/homepage/index.php
@@ -28,14 +28,14 @@ if(isset($_SESSION['users_id'])){
         <a href = "../scanner">
             <div class="box color-3">
                 <h2 class="color-white">Scan barcode</h2>
-                <img src="../images/barcode">
+                <img src="../images/barcode.png">
             </div>
         </a>
 
         <a href="../history/index.php">
             <div class="box color-6">
                 <h2 class="color-white">Zoek product</h2>
-                <img src="../images/search">
+                <img src="../images/search.png">
             </div>
         </a>
 
